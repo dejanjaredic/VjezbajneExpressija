e88ec3c15b0d386eb3ef0f773f7b68b08ccfda45
yankeejunior <dekijaredic@gmail.com>
2019-05-05 12:15:36 +0200

    First commit

---
+++ VjezbanjeExpresija/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace VjezbanjeExpresija
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TestingClass.Test(12, 7));
+            Console.WriteLine(TestingClass.BinExp());
+            Console.WriteLine(TestingClass.FinalBinExp());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ VjezbanjeExpresija/TestingClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Net.NetworkInformation;
+using System.Text;
+
+namespace VjezbanjeExpresija
+{
+    public class TestingClass
+    {
+        public static bool Test(int value1, int value2)
+        {
+            Func<int, bool> test = i => i > value2;
+            return test(value1);
+        }
+
+        public static Expression BinExp()
+        {
+            Expression<Func<int, bool>> exp = i => i > 5;
+            BinaryExpression binary = (BinaryExpression)exp.Body;
+            Console.WriteLine(binary.Left);
+            Console.WriteLine(binary.NodeType);
+            Console.WriteLine(binary.Right);
+            return binary;
+        }
+        // Func<int, bool> test = i => i > 5
+        public static Expression FinalBinExp()
+        {
+            ConstantExpression constExp =  Expression.Constant(5, typeof(int));
+            ParameterExpression parExp = Expression.Parameter(typeof(int), "i");
+            BinaryExpression binExp = Expression.MakeBinary(ExpressionType.GreaterThan, parExp, constExp);
+            Expression<Func<int, bool>> lambda = 
+                Expression.Lambda<Func<int, bool>>(binExp, parExp);
+            //return constExp.NodeType;
+            return lambda;
+        }
+    }
+}
